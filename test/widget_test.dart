0bbc237b412bb3e5730df3526352139e59d1a500
thessalenejean-louis <tjeanlouis@ippon.fr>
2024-03-30 14:45:32 +0100

    :white_check_mark: Minor fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fluffy_budget/app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp("Test"));
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
